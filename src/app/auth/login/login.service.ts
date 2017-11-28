decd39035cb4809763b8915d7604076f2d96ce61
prasadsrg <prasad.srg@gmail.com>
2017-11-28 19:09:17 +0530

    Revert "Revert "no message""
    This reverts commit 6dcdd411e5b4002052c3457ce7643fdb63db0787.

---
+++ src/app/auth/login/login.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Props } from '../../apex/common/props';
+import { AppService } from '../../shared/service/app.service';
+
+@Injectable()
+export class LoginService {
+    constructor(private appService: AppService)    {
+        
+     }
+     
+}
