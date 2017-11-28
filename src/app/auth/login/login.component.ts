75bb6bf44d7d75896f635990da849c4181c74a52
prasadsrg <prasad.srg@gmail.com>
2017-11-28 19:27:08 +0530

    commit for login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, HostListener} from '@angular/core';
+import { MyForm } from './login.form';
+import { AnimationService } from '../../shared/service/animation.service';
+import { AppService } from '../../shared/service/app.service';
+import { User } from '../../apex/entities/user'; 
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  host: AnimationService.host,
+  animations: AnimationService.page
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
+  myForm: any = MyForm.init();
+  auth: User;
+  showorhide: string;
+  isVisible: string;
+  error: any;
 
-  constructor() { }
+  constructor() {
+     MyForm.edit(this.myForm);
+     this.init();
+  }
+
 
   ngOnInit() {
   }
+  
+  init() {
 
+    this.auth = new User();
+  }
 }
