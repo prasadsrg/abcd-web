9a7a7e1f209f1dbd382560c985f11aa169993d84
LAPTOP-MJICMDF2\Tharun <tharundillikar@gmail.com>
2017-11-28 19:37:40 +0530

    menu

---
+++ src/app/auth/menu/menu.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit,ViewChild, HostListener } from '@angular/core';
+import { MatSidenav } from '@angular/material';
+import { Menu } from '../../shared/common/interfaces';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  @ViewChild('sidenav') sidenav: MatSidenav;
+  navMode = 'side';
+  menuList: any[];
+  activeMenuObject: any;
+  
+  constructor() { 
+    console.log(this.menus());
+    this.menuList = this.menus();
+  }
+
+
+  ngOnInit() {
+    // if (window.innerWidth < 768) {
+    //   this.navMode = 'over';
+    //   console.log('dasdas')
+      
+    // }
+  }
+  // @HostListener('window:resize', ['$event'])
+  // onResize(event) {
+  //     if (event.target.innerWidth < 768) {
+  //         this.navMode = 'over';
+  //         this.sidenav.close();
+  //         console.log('hasdfj')
+  //     }
+  //     if (event.target.innerWidth > 768) {
+  //        this.navMode = 'side';
+  //        this.sidenav.open();
+
+  //     }
+  // }
+  menus() {
+    return [
+     
+      {
+        "id": null,
+        "name": "Home",
+        "link": "/",
+        "icon": "alarm"
+      },
+      {
+        "id": null,
+        "name": "Login",
+        "link": "/auth/login",
+        "icon": "accessibility"
+      },
+      {
+        "id": null,
+        "name": "Register",
+        "link": "/auth/register",
+        "icon": "alarm"
+      }
+    ]
+  }
+  // navigate(item) {
+  //   this.activeMenuObject = item.name;
+  //   event.stopPropagation();
+  //   console.log(item.url)
+  // }
+
+ 
+}
