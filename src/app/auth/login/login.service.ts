26e7845a3eb349f708f9b0f84a8637c3c0d3e767
srgpr <prasad.srg@gmail.com>
2017-11-29 15:28:14 +0530

    commit envirorment

--- src/app/auth/login/login.service.ts
+++ src/app/auth/login/login.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Props } from '../../apex/common/props';
 import { AppService } from '../../shared/service/app.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class LoginService {
-    constructor(private appService: AppService)    {
+    private host = Props.API_END_POINT;
+    private auth_url = 'auth'
+    constructor(private http: HttpClient, private appService: AppService)    {
         
      }
-     
+     login(data: any) {
+         //this.http.post(this.host);
+     }
 }
