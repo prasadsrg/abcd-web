5f7abedc1e5549e70aee52242b24d2928247caaa
prasadsrg <prasad.srg@gmail.com>
2017-12-04 22:24:21 +0530

    commit for myprofile

--- src/app/home/profile/myprofile/myprofile.component.ts
+++ src/app/home/profile/myprofile/myprofile.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { AnimationService } from '../../../shared/service/animation.service';
 import { MyProfileForm } from './myprofile.form';
-
+import { ProfileService } from './../profile.service';
+import { Profile } from '../../../apex/entities/profile.entity';
 @Component({
   selector: 'app-myprofile',
   templateUrl: './myprofile.component.html',
   styleUrls: ['./myprofile.component.scss']
 })
 export class MyprofileComponent implements OnInit {
+  profile: Profile = new Profile();
   myForm: any = MyProfileForm.init();
-  constructor() {
+  constructor(private profileService: ProfileService) {
     MyProfileForm.edit(this.myForm);
-    // this.init();
+    this.entity(this.profileService.getCurrentProfileId());
    }
 
   ngOnInit() {
+   
   }
   // init() {
 
   //   this.auth = new User();
   // }
+
+  entity(id: string){
+    this.profileService.getProfile(id).subscribe( (data: Profile) => {
+      this.profile = data;
+    })
+  }
 
 }
--- src/app/home/profile/profile.service.ts
+++ src/app/home/profile/profile.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Props } from '../../apex/common/props';
+import { AppService } from '../../shared/service/app.service';
+import { HttpClient } from '@angular/common/http';
+import { Storage }from '../../shared/utils/storage';
 
 @Injectable()
 export class ProfileService {
+  private host = Props.API_END_POINT;
+  private url: string = '';
+  constructor(private http: HttpClient, private appService: AppService) { 
 
-  constructor() { }
-
+  }
+  getParamId(){
+    return this.appService.getParam('id');
+  }
+  getCurrentProfileId(){
+    return Storage.getSessionUser().id;
+  }
+  getProfile(data: any){
+    this.appService.showLoader(true);
+    this.url = this.host+"profile/"+data;
+    return this.http.get(this.url);
+  }
 }
