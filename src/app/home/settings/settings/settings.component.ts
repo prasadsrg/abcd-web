09e7d6fc331ace6095148629906e49f586fa37b6
LAPTOP-MJICMDF2\Tharun <tharundillikar@gmail.com>
2017-12-11 20:15:12 +0530

    settings

--- src/app/home/settings/settings/settings.component.ts
+++ src/app/home/settings/settings/settings.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import {SettingsService} from '../settings.service';
+import { ApexService } from './../../../shared/service/apex.service';
 
+import {Storage} from './../../../shared/utils/storage'
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  userId:any;
+  branches:any;
+  branchId:any;
+  elem:any;
+  branchstatus:any;
+  isActive:boolean;
 
-  constructor() { }
+  branchData = {
+      id:null,
+      branch:{
+        id:null
+      }
+  }
+  constructor(private settingsservice: SettingsService, private apexservice: ApexService) {
+    this.branchesDataLoad();
+
+   }
 
   ngOnInit() {
   }
-
+  branchValue(value){
+    this.userId = Storage.getSessionUser().id;
+    this.branchData.id = this.userId
+    this.branchData.branch.id = value;
+    console.log(this.branchData); 
+  }
+  saveBranch(){
+    this.settingsservice.saveBranch(this.branchData).subscribe((data)=>{
+      // console.log(data)
+    })
+  }
+  branchesDataLoad(){
+    this.settingsservice.getBranches().subscribe((data)=>{
+     this.branches = data
+     console.log(this.branches)
+    this.branchId = Storage.getBranch();
+    console.log(this.branchId)    
+     this.branches.forEach(element => {
+      this.elem = element.id;
+      this.branchstatus = this.branchId.id;
+      
+      this.isActive = this.elem == this.branchstatus;
+      console.log(this.isActive)
+      
+     });
+      
+    })
+  }
 }
