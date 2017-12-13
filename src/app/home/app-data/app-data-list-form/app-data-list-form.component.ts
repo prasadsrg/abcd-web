22afcc044edaec96ac4c079b9218d76ca85e8bc6
LAPTOP-MJICMDF2\Tharun <tharundillikar@gmail.com>
2017-12-13 17:44:21 +0530

    commit

--- src/app/home/app-data/app-data-list-form/app-data-list-form.component.ts
+++ src/app/home/app-data/app-data-list-form/app-data-list-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-app-data-list-form',
+  selector: 'app-data-list-form',
   templateUrl: './app-data-list-form.component.html',
   styleUrls: ['./app-data-list-form.component.scss']
 })
